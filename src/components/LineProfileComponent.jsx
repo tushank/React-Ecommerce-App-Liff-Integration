067902b3ccd941da4d28dce31f5520b21e294bc6
tushank <tushank.shethe@gmail.com>
2023-10-17 15:49:27 +0530

    test send message functionality

--- src/components/LineProfileComponent.jsx
+++ src/components/LineProfileComponent.jsx
@@ -1,9 +1,31 @@
-import React, { useState, useEffect } from 'react';
-import liff from '@line/liff';
+import React, { useState, useEffect } from "react";
+import liff from "@line/liff";
 
 function LineProfileComponent() {
-  const myLiffId = '2001073406-74eKPLm9';
+  const myLiffId = "2001073406-74eKPLm9";
   const [profile, setProfile] = useState(null);
+  const [message, setMessage] = useState("");
+
+  const sendMessageToFriend = (message) => {
+    if (liff.isInClient()) {
+      liff
+        .sendMessages([
+          {
+            type: "text",
+            text: message,
+          },
+        ])
+        .then(() => {
+          console.log("Message sent");
+        })
+        .catch((err) => {
+          console.error("Error sending message:", err);
+        });
+    } else {
+      // User is accessing the app outside of the LINE app, handle accordingly.
+      console.log("User is not in the LINE app");
+    }
+  };
 
   useEffect(() => {
     async function fetchProfile() {
@@ -12,7 +34,7 @@ function LineProfileComponent() {
           const userProfile = await liff.getProfile();
           setProfile(userProfile);
         } catch (error) {
-          console.error('Failed to fetch user profile:', error);
+          console.error("Failed to fetch user profile:", error);
         }
       } else {
         liff.login();
@@ -27,7 +49,7 @@ function LineProfileComponent() {
           fetchProfile();
         },
         (error) => {
-          console.error('LIFF initialization failed', error);
+          console.error("LIFF initialization failed", error);
         }
       );
     } else {
@@ -42,6 +64,15 @@ function LineProfileComponent() {
           <img src={profile.pictureUrl} alt={profile.displayName} />
           <p>{profile.displayName}</p>
           <p>{profile.statusMessage}</p>
+
+          <input
+            type="text"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
+          <button onClick={() => sendMessageToFriend(message)}>
+            Send Message
+          </button>
         </div>
       ) : (
         <p>Loading...</p>
